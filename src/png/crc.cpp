f2feefa7d32a87ec1a1d96405ae939c7f6ade1a4
Marco <46133335+Marco4413@users.noreply.github.com>
2023-08-02 12:15:44 +0200

    Moved CRC constructor to crc.cpp

--- src/png/crc.cpp
+++ src/png/crc.cpp
@@ -1,8 +1,24 @@
 #include "png/crc.h"
 
+// Code taken from http://www.libpng.org/pub/png/spec/1.2/PNG-CRCAppendix.html
+// I am not smart enough to come up with my own solution.
+
 const PNG::CRC PNG::CRC::m_Instance = PNG::CRC();
 
-// Code taken from http://www.libpng.org/pub/png/spec/1.2/PNG-CRCAppendix.html
+PNG::CRC::CRC()
+{
+    for (size_t n = 0; n < 256; n++) {
+        uint32_t c = n;
+        for (size_t k = 0; k < 8; k++) {
+            if (c & 1)
+                c = 0xedb88320L ^ (c >> 1);
+            else
+                c = c >> 1;
+        }
+        m_Table[n] = c;
+    }
+}
+
 uint32_t PNG::CRC::Update(uint32_t crc, const void* _buf, size_t bufLen)
 {
     const uint8_t* buf = (uint8_t*)_buf;
@@ -19,7 +35,6 @@ uint32_t PNG::CRC::Update(uint32_t crc, uint32_t value)
     return Update(crc, buf, 4);
 }
 
-// Code taken from http://www.libpng.org/pub/png/spec/1.2/PNG-CRCAppendix.html
 uint32_t PNG::CRC::Calculate(const void* buf, size_t bufLen)
 {
     return ~Update(~0, buf, bufLen);
--- src/png/crc.h
+++ src/png/crc.h
@@ -10,25 +10,13 @@ namespace PNG
     class CRC
     {
     public:
+        // Code taken from http://www.libpng.org/pub/png/spec/1.2/PNG-CRCAppendix.html
         static uint32_t Update(uint32_t crc, const void* buf, size_t bufLen);
         static uint32_t Update(uint32_t crc, uint32_t val);
         static uint32_t Calculate(const void* buf, size_t bufLen);
     
     private:
-        // Code taken from http://www.libpng.org/pub/png/spec/1.2/PNG-CRCAppendix.html
-        CRC() {
-            for (size_t n = 0; n < 256; n++) {
-                uint32_t c = n;
-                for (size_t k = 0; k < 8; k++) {
-                    if (c & 1)
-                        c = 0xedb88320L ^ (c >> 1);
-                    else
-                        c = c >> 1;
-                }
-                m_Table[n] = c;
-            }
-        }
-
+        CRC();
         uint32_t m_Table[256]{0};
         static const CRC m_Instance;
     };
