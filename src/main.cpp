5679d3743c5d57eecc4ea26ea435205641fc39a7
Marco <46133335+Marco4413@users.noreply.github.com>
2023-08-28 20:02:22 +0200

    Use PNG_PRINTF instead of direct calls to fmt

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
         auto pngcode = (func)(__VA_ARGS__); \
         bool pngok = pngcode == PNG::Result::OK; \
         if (!pngok) { \
-            fmt::print(fmt::fg(fmt::color::red), "{}:{}: Assert (pngok) failed: {} -> {}\n", __FILE__, __LINE__, #func, PNG::ResultToString(pngcode)); \
+            PNG_PRINTF(fmt::fg(fmt::color::red), "{}:{}: Assert (pngok) failed: {} -> {}\n", __FILE__, __LINE__, #func, PNG::ResultToString(pngcode)); \
             std::exit(1); \
         } \
     } while (0)
@@ -28,9 +28,9 @@ void bench(const char* name, const std::function<void()>& fun, size_t samples = 
     }
 
     if (samples == 1)
-        fmt::print(fmt::fg(fmt::color::yellow), "{} took {}us.\n", name, totalTime);
+        PNG_PRINTF(fmt::fg(fmt::color::yellow), "{} took {}us.\n", name, totalTime);
     else
-        fmt::print(fmt::fg(fmt::color::yellow), "{} took an average of {}us on {} samples.\n", name, totalTime / samples, samples);
+        PNG_PRINTF(fmt::fg(fmt::color::yellow), "{} took an average of {}us on {} samples.\n", name, totalTime / samples, samples);
 }
 
 int main(int argc, char** argv)
@@ -40,7 +40,7 @@ int main(int argc, char** argv)
     assert(*argv++ != NULL);
     const char* filePath = *argv++;
     if (filePath == NULL) {
-        fmt::println("Please specify a file to open.");
+        PNG_PRINTF("Please specify a file to open.\n");
         return 1;
     }
 
@@ -88,48 +88,15 @@ int main(int argc, char** argv)
         ASSERT_OK(img.WriteMT, outStream, exportSettings);
     });
 
-    /*
-    Commit @4f560aa
-    These benchmarks are done using the O3 optimized build, and their timings are the average of 10 runs.
-    Writing has been done using ColorType::RGBA, 8bit depth, CompressionLevel::Default and InterlaceMethod::None.
-    Only 2 images were used, and Writing benchmarks are only stated once per image.
-    2560x1440 (449 IDAT, NI) ->
-        R-ST 290ms
-        R-MT 212ms
-        W-ST 1.479s
-        W-MT 914ms
-    2560x1440 (511 IDAT, A7) ->
-        R-ST 341ms
-        R-MT 232ms
-    800x600 (1 IDAT, NI) ->
-        R-ST 28ms
-        R-MT 18ms
-        W-ST 125ms
-        W-MT 68ms
-    800x600 (30 IDAT, NI) ->
-        R-ST 32ms
-        R-MT 24ms
-    800x600 (39 IDAT, A7) ->
-        R-ST 37ms
-        R-MT 27ms
-    
-    R: Reading
-    W: Writing
-    ST: Single Threaded
-    MT: Multi Threaded
-    NI: No Interlace
-    A7: Adam7 Interlace
-    */
-
     size_t x = img.GetWidth() / 2;
     size_t y = img.GetHeight() / 2;
     const auto& pixel = img[y][x];
-    
-    fmt::println("Pixel at {}, {}", x, y);
-    fmt::println("  R: {}", (size_t)(pixel.R*255));
-    fmt::println("  G: {}", (size_t)(pixel.G*255));
-    fmt::println("  B: {}", (size_t)(pixel.B*255));
-    fmt::println("  A: {}", (size_t)(pixel.A*255));
+
+    PNG_PRINTF("Pixel at {}, {}\n", x, y);
+    PNG_PRINTF("  R: {}\n", (size_t)(pixel.R*255));
+    PNG_PRINTF("  G: {}\n", (size_t)(pixel.G*255));
+    PNG_PRINTF("  B: {}\n", (size_t)(pixel.B*255));
+    PNG_PRINTF("  A: {}\n", (size_t)(pixel.A*255));
 
     return 0;
 }
