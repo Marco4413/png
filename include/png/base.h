e1f2e277d32a9e8b68aee8a9c449e7fd76a7a9ce
Marco <46133335+Marco4413@users.noreply.github.com>
2023-08-30 21:43:54 +0200

    PNG_ASSERT/UNREACHABLE should trigger a breakpoint before terminating

--- include/png/base.h
+++ include/png/base.h
@@ -6,6 +6,7 @@
 #include <cinttypes>
 #include <cstdlib>
 #include <cstring>
+#include <csignal>
 #include <vector>
 
 #ifdef PNG_NO_LOGGING
@@ -25,10 +26,24 @@
 
 #ifdef PNG_DEBUG
 
+// PNG_DEBUG_BREAK
+#if defined(_MSC_VER)
+  // MSC has a __debugbreak intrinsic that can be called for breakpoints
+  #include <intrin.h>
+  #define PNG_DEBUG_BREAK() __debugbreak()
+// PNG_DEBUG_BREAK
+#elif defined(SIGTRAP)
+  // POSIX systems SHOULD have the SIGTRAP signal for debug breakpoints
+  #define PNG_DEBUG_BREAK() std::raise(SIGTRAP)
+#else // PNG_DEBUG_BREAK
+  #define PNG_DEBUG_BREAK() (void)(0)
+#endif // PNG_DEBUG_BREAK
+
 #define PNG_ASSERT(cond, msg) \
     do { \
         if (!(cond)) { \
             PNG_PRINTF(stderr, fmt::fg(fmt::color::red), "{}:{}: Assert ({}) failed: {}\n", __FILE__, __LINE__, #cond, msg); \
+            PNG_DEBUG_BREAK(); \
             std::exit(1); \
         } \
     } while (0)
@@ -38,6 +53,7 @@
         if (!(cond)) { \
             PNG_PRINTF(stderr, fmt::fg(fmt::color::red), "{}:{}: Assert ({}) failed: {}\n", __FILE__, __LINE__, #cond, \
                 fmt::format(sfmt, __VA_ARGS__ )); \
+            PNG_DEBUG_BREAK(); \
             std::exit(1); \
         } \
     } while (0)
@@ -51,17 +67,20 @@
 
 #else // PNG_DEBUG
 
+#define PNG_DEBUG_BREAK() (void)(0)
+
 #define PNG_ASSERT(cond, msg) do { (void)(cond); (void)(msg); } while (0)
 #define PNG_ASSERTF(cond, fmt, ...) do { (void)(cond); (void)(fmt); } while (0)
 
-#define PNG_LDEBUG(msg) do { (void)(msg); } while (0)
-#define PNG_LDEBUGF(fmt, ...) do { (void)(fmt); } while (0)
+#define PNG_LDEBUG(msg) (void)(msg)
+#define PNG_LDEBUGF(fmt, ...) (void)(fmt)
 
 #endif // PNG_DEBUG
 
 #define PNG_UNREACHABLE(msg) \
     do { \
         PNG_PRINTF(stderr, fmt::fg(fmt::color::red), "{}:{}: Unreachable: {}\n", __FILE__, __LINE__, msg); \
+        PNG_DEBUG_BREAK(); \
         std::exit(1); \
     } while (0)
 
@@ -69,6 +88,7 @@
     do { \
         PNG_PRINTF(stderr, fmt::fg(fmt::color::red), "{}:{}: Unreachable: {}\n", __FILE__, __LINE__, \
             fmt::format(sfmt, __VA_ARGS__ )); \
+        PNG_DEBUG_BREAK(); \
         std::exit(1); \
     } while (0)
 
